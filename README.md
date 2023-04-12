b8a179dc0b5dc0e0084d2817bdc8beccd2f2813b
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-04-12 16:11:21 +0500

    add functionalty to post image dynamicaly using active storage

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ created new project successfuly
 DRY Dont repeat you self
 -re-factoring
 -code redundancy
+Add boostrap
+design home page using bootstrap
+add image dynamicaly using active storage
 * ...
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,7 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def index 
         @articles = Article.all
+
     end
     def new
 @article = Article.new
@@ -16,7 +17,8 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
     end
 
     def create
-        @article = Article.new(set_params)
+        @article = Article.new(article_params)
+
         if @article.save
             flash[:notice] = "Article was created successfully."
             redirect_to @article
@@ -27,7 +29,7 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
     end
 
     def update
-       if @article.update(set_params)
+       if @article.update(article_params)
         flash[:notice] = "Article was updated successfuly"
         redirect_to @article
         else
@@ -49,8 +51,9 @@ def set_article
     @article = Article.find(params[:id]) 
 end
 
-def set_params
-    params.require(:article).permit(:title, :description)
-end
+
+def article_params
+    params.require(:article).permit(:title, :description, :image)
+  end
 
 end
