5c4a786bcfda98c8b84790106b1255c592b5444b
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-04-13 15:10:56 +0500

    add suer validtaion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..25 }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }, 
+    length: { maximum: 105 }
 
-end
+  end
+  
