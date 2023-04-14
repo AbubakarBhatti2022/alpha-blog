ff5f23dc4bd5ffdf3f700a87ecac6d305a722fa4
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-04-14 14:16:30 +0500

    add validation in user model to set email in downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
   class User < ApplicationRecord
-      has_many :articles  
+      
+    before_save { self.email = email.downcase }
       validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..25 }
       validates :email, presence: true, uniqueness: { case_sensitive: false }, 
       format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }, 
       length: { maximum: 105 }
+      has_many :articles  
     
   end
     
