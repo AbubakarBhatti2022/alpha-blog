c86f7424e5165ab2681683f8cf47695f98dd4362
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-04-14 17:20:00 +0500

    add user create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
   class User < ApplicationRecord
       
     before_save { self.email = email.downcase }
-      validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..25 }
+      validates :username, presence: true, uniqueness: { case_sensitive: false }
       validates :email, presence: true, uniqueness: { case_sensitive: false }, 
       format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }, 
       length: { maximum: 105 }
