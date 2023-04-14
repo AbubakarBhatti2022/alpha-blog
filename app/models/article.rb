4e0259cb606de4a81261a4b661ac86c404aba36e
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-04-14 12:10:34 +0500

    add one to many association between user and articles from back-end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
     validates :description, presence: true, length: { minimum: 10, maximum: 300 }
     has_one_attached :image
+    validates :image, presence: { message: "must be present" }
+    belongs_to :user
    
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
-class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..25 }
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, 
-    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }, 
-    length: { maximum: 105 }
-
+  class User < ApplicationRecord
+      has_many :articles  
+      validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..25 }
+      validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+      format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }, 
+      length: { maximum: 105 }
+    
   end
-  
+    
