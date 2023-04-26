4d44b80fba1b6f5f614462bb48b5dd21938e0d9b
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-04-26 15:06:06 +0500

    addedd current_user and logged_in? helper methods modified the naviation and display links based on logged in state modified the user controller create action so users are logged in one they signup and addedd set_user method use to before action for show, edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
    
+
+    before_action :set_user, only: [:show, :edit, :update]
     
     def show 
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 4)
     end
 
@@ -15,11 +16,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
         flash[:notice] = "Your Account was successfuly Updated"
         redirect_to @user
@@ -31,8 +30,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome To The Alpha Blog, #{@user.username} You Have Successfully SignUp"
-            redirect_to articles_path
+            redirect_to users_path
             else
                 render 'new'
 
@@ -42,6 +42,11 @@ class UsersController < ApplicationController
 private 
 def user_params
     params.require(:user).permit(:username, :email, :password, :image )
+end
+
+def set_user
+    @user = User.find(params[:id])
+
 end
 
   end
