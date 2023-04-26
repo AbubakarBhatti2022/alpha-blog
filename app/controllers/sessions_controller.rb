0396d4a5a277f9f3e1254913bd41d998703b8fc7
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-04-26 12:37:55 +0500

    complete create and destroy actions for sessions controller to log user in and out and addedd login and logout links to navigation partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,24 @@ def new
 end
 
 def create
-byebug
+user = User.find_by(email: params[:session][:email].downcase)
+if user && user.authenticate(params[:session][:password])
+    session[:user_id] = user.id
+    flash[:notice] = "Logged in successfuly"
+    redirect_to user
+
+else
+flash.now[:alert] = "Invalid email/password combination"
+render 'new'
+end
+
 end
 
 def destroy
+
+    session[:user_id] = nil
+    flash[:notice] = "Logged Out"
+    redirect_to root_path
 
 end
 
