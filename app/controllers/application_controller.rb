7816be1f4ed63c4dd39eb682ff29d3e1c5b6a497
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-04-26 19:04:53 +0500

    add restrict action at controller in articles only authentics article creater delete and edit and update the article by using url path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
     helper_method :current_user, :logged_in?
-    
+
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     
@@ -11,5 +11,14 @@ class ApplicationController < ActionController::Base
         !!current_user
     end
     
+    def require_user
+        if !logged_in?
+        flash[:notice] = "you must be logged in to perform that action"
+        redirect_to login_path
+        end
+    end
+
+   
 
 end
+
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
 before_action :set_article, only: [:show, :edit, :update, :destroy]
+before_action :require_user, except: [:show, :index]
+before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
     end
@@ -56,5 +58,13 @@ end
 def article_params
     params.require(:article).permit(:title, :description, :image)
   end
+
+
+  def require_same_user
+    if current_user != @article.user
+        flash[:notice] = "You can only edit and delete your own article"
+        redirect_to @article
+    end
+end
 
 end
