e30599c7c83b9f41f0a9e5a12b64d4a4dc98ab74
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-04-27 12:54:53 +0500

    changing in navbar to show active user dropdown and add delete module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
    
 
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     
     def show 
         @articles = @user.articles.paginate(page: params[:page], per_page: 4)
@@ -18,6 +18,13 @@ class UsersController < ApplicationController
     end
 
     def edit
+    end
+
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "Account and all Associated articles successfuly delete"
+        redirect_to root_path
     end
 
     def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@
       validates :email, presence: true, uniqueness: { case_sensitive: false }, 
       format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }, 
       length: { maximum: 105 }
-      has_many :articles  
+      has_many :articles, dependent: :destroy  
       has_secure_password
       has_one_attached :image
     validates :image, presence: { message: "must be present" }
