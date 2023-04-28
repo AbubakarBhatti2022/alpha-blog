20640a3c5a4e440db2fb848720f8a3cd2c7bedbb
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-04-28 16:11:38 +0500

    addedd functional-test for creation action and addedd new category creation form and also cpmpleted/addedd new and create action in categories-controller with category-params private method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,18 @@
 class CategoriesController < ApplicationController
 
     def new 
+        @category = Category.new
+    end
 
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was Successfully Created"
+            redirect_to @category
+            
+        else
+            render 'new'
+        end
     end
 
     def Index
@@ -12,5 +23,15 @@ class CategoriesController < ApplicationController
 
     end
 
+
+    private
+
+    def category_params
+
+        params.require(:category).permit(:name)
+
+    end
+
     
 end
+
