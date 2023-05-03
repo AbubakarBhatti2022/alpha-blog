24827df7237f7f80787684322aeb723dbc7625e6
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-05-03 12:57:02 +0500

    add category edit and category delete functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,26 @@ class CategoriesController < ApplicationController
         @category = Category.new
     end
 
+    def edit
+        @category = Category.find(params[:id])
+      end
+
+      def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice] = "Category name Updated Successfuly"
+          redirect_to @category
+        else
+          render 'edit'
+        end
+      end
+
+      def destroy
+        @category = Category.find(params[:id])
+        @category.destroy
+        redirect_to @category
+      end
+
     def create
         @category = Category.new(category_params)
         if @category.save
